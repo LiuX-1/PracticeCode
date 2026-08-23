fd0626bb8e62d8f90696bf95e6118fc34f92e5a0
liuxu <1097593639@qq.com>
2026-08-24 01:32:55 +0800

    结构型模式-装饰模式

--- test.cpp
+++ test.cpp
@@ -1633,3 +1633,90 @@ int main() {
     // Derived d3(b2);     // ❌ 编译错误：同样不行
     return 0;
 }
+
+
+
+//结构型模式---装饰模式
+
+class Car
+{
+public:
+	virtual void show() = 0;
+};
+
+class RunCar : public Car 
+{
+public:
+	virtual void show()
+	{
+		cout << "可以跑" << endl;
+	}
+protected:
+private:
+};
+
+
+class SwimCarDirector : public Car
+{
+public:
+	SwimCarDirector(Car *car)
+	{
+		m_car = car;
+	}
+	void swim()
+	{
+		cout << "可以游" << endl;
+	}
+	virtual void show()
+	{
+		m_car->show();
+		swim();
+	}
+protected:
+private:
+	Car *m_car;
+};
+
+class  FlyCarDirector : public Car
+{
+public:
+	FlyCarDirector(Car *car)
+	{
+		m_car = car;
+	}
+	void fly()
+	{
+		cout << "可以飞" << endl;
+	}
+	virtual void show()
+	{
+		m_car->show();
+		fly();
+	}
+
+private:
+	Car *m_car;    //has-a 关系
+};
+
+void main()
+{
+	Car * mycar = NULL;
+	mycar = new RunCar;
+	printf("-----000\n");
+	mycar->show();
+
+	printf("-----aaa\n");
+
+	FlyCarDirector *flycar = new FlyCarDirector(mycar);
+	flycar->show();
+
+	printf("-----bbb\n");
+	SwimCarDirector *swimcar = new SwimCarDirector(flycar);
+	swimcar->show();
+	
+	delete swimcar;
+	delete flycar;
+	delete mycar;
+	system("pause");
+	return ;
+}
